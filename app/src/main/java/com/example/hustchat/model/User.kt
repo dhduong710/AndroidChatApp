e10940919b2821b37337cd3b676f27379b7c9395
Duong <155739353+duongdh710@users.noreply.github.com>
2026-01-02 23:31:26 +0700

    Can see friend's status (online,...) and fix:can display name,.. for group

--- app/src/main/java/com/example/hustchat/model/User.kt
+++ app/src/main/java/com/example/hustchat/model/User.kt
@@ -5,5 +5,8 @@ data class User(
     val username: String = "",
     val email: String = "",
     val phoneNumber: String = "",
-    val avatarUrl: String = "" // Default is empty, the UI will automatically handle the default image.
+    val avatarUrl: String = "", // Default is empty, the UI will automatically handle the default image
+
+    val status: String = "offline", // "online" or "offline"
+    val lastSeen: Long = 0
 )
